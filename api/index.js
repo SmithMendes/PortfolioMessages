fecf9fdca43119e0b7e32ada36c862a9042c5432
smithmendes4 <smithmendes4@gmail.com>
2026-04-16 21:36:58 +0530

    Fix Vercel setup with Zero-Config structure

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json()); // To parse JSON bodies
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Explicitly serve dashboard on root
 app.get('/', (req, res) => {
